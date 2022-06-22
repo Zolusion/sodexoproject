33022114093c63118683eace0976d3bf76aa2ba2
Furkan Baykal <furkanbaykal2014@hotmail.com>
2022-06-22 19:43:22 +0200

    first commit

---
+++ vacatures.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sodexo Vacatures</title>
+</head>
+<body>
+    <!-- login bar -->
+    <div class="sodexo-loginbar">
+
+    </div>
+
+    <!-- image -->
+    <div class="sodexo-image">
+
+    </div>
+
+    <!-- navbar -->
+    <div class="sodexo-navbar">
+
+    </div>
+
+    <!-- text -->
+    <div class="sodexo-text">
+
+    </div>
+
+    <!-- vacatures -->
+    <div class="sodexo-vacatures">
+
+    </div>
+
+    <!-- text -->
+    <div class="sodexo-text-2">
+
+    </div>
+
+    <!-- logo's van klanten -->
+    <div class="sodexo-logos">
+
+    </div>
+
+    <!-- menu -->
+    <div class="sodexo-menu">
+
+    </div>
+</body>
+</html>
